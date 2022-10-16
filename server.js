7a1368f5531a6b8241b97a4cf8cf1029276fc72b
Chinyelu Lyon <chinyelulyon@gmail.com>
2022-10-16 18:41:42 +0100

    dont add duplicate tracks

--- server.js
+++ server.js
@@ -8,16 +8,17 @@ const app = express();
 
 const dbURI =
   "mongodb+srv://Admin:vEjVQ36L9BZBmto2@cluster0.3wegskq.mongodb.net/curve?retryWrites=true&w=majority";
+
 mongoose
   .connect(dbURI)
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
-const contractExists = (contractName, next) => {
+const contractExistsOld = (contractName, next) => {
   Contract.find({ name: contractName })
     .then((result) => {
       if (result.length > 0) {
-        console.log(result);
+        console.log(`${contractName} found`);
         next(true);
       } else {
         console.log(`${contractName} not found`);
@@ -29,57 +30,101 @@ const contractExists = (contractName, next) => {
     });
 };
 
-const handleAliases = (aliasList) => {
-  return aliasList.split(";");
+const contractExists = (contractName) => {
+  return new Promise(async (resolve, reject) => {
+    Contract.find({ name: contractName })
+      .then((result) => {
+        if (result.length > 0) {
+          resolve(true);
+          console.log(`${contractName} found`);
+        } else {
+          resolve(false);
+          console.log(`${contractName} not found`);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 };
 
-const createTracksFromTable = async (res, table) => {
+const trackExists = (trackTitle) => {
+  return new Promise(async (resolve, reject) => {
+    Track.find({ title: trackTitle })
+      .then((result) => {
+        if (result.length > 0) {
+          resolve(true);
+          console.log(`${trackTitle} found`);
+        } else {
+          resolve(false);
+          console.log(`${trackTitle} not found`);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
+
+const createTracksFromTable = async (table) => {
   table.forEach((t) => {
-    contractExists(t.Contract, (found) => {
+    contractExists(t.Contract).then((found) => {
       let contractName;
 
       if (found) {
         contractName = t.Contract;
       }
 
-      const newTrack = new Track({
-        title: t.Title,
-        version: t.Version,
-        artist: t.Artist,
-        ISRC: t.ISRC,
-        pLine: t["P Line"],
-        aliases: handleAliases(t.Aliases),
-        contract: contractName,
+      trackExists(t.Title).then((found) => {
+        if (!found) {
+          const newTrack = new Track({
+            title: t.Title,
+            version: t.Version,
+            artist: t.Artist,
+            ISRC: t.ISRC,
+            pLine: t["P Line"],
+            aliases: t.Aliases.split(";"),
+            contract: contractName,
+          });
+          newTrack
+            .save()
+            .then((result) => {
+              console.log("Saved Track!");
+            })
+            .catch((err) => console.log(err));
+        }
       });
-      newTrack
-        .save()
-        .then((result) => {
-          res.send(result);
-        })
-        .catch((err) => console.log(err));
     });
   });
 };
 
 app.get("/", (req, res) => {
+  const contractName = "Contract 1";
+
+  contractExists(contractName).then((found) => {
+    if (!found) {
+      const contract = new Contract({
+        name: contractName,
+      });
+
+      contract
+        .save()
+        .then(() => {
+          console.log("All good");
+        })
+        .catch((err) => console.log(err));
+    }
+    res.redirect("http://localhost:3000/add-from-file");
+  });
+});
+
+app.get("/add-from-file", (req, res) => {
   const workbook = xlsx.readFile(__dirname + "/files/Track Import Test.xlsx");
 
   workbook.SheetNames.forEach((sheet) => {
     tableData = xlsx.utils.sheet_to_json(workbook.Sheets[sheet]).slice(1);
 
-    createTracksFromTable(res, tableData);
+    createTracksFromTable(tableData);
+    res.send("Date: " + new Date());
   });
-});
-
-app.get("/add-contract", (req, res) => {
-  const contract = new Contract({
-    name: "Contract 1",
-  });
-
-  contract
-    .save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => console.log(err));
 });
